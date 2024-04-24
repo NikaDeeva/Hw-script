3248d4a7eef17598cf8ea15a2a24cc6d7125eb65
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-04-24 20:59:40 +0200

    ok

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1
+const c = 3;
+f = 2 * c + 32;
+console.log(f);
+
+// 2
+const d = 30;
+const h = 24;
+const min = 60;
+console.log(d * h);
+console.log(d * h * min);
+
+// 3
+let hp = 100;
+let energy = 100;
+hp -=20;
+energy -= 50;
+console.log(hp);
+console.log(energy);
+
+// 4
+const milk = 30;
+const potato = 20;
