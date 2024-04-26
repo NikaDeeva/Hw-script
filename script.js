784e3066a2ea9284464f9652d8122740bf67b125
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-04-26 20:01:26 +0200

    ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+"use strict"
+
 // 1
 const c = 3;
-f = 2 * c + 32;
+const f = 2 * c + 32;
 console.log(f);
 
 // 2
@@ -24,3 +26,21 @@ const potato = 20;
 const apple = 25;
 const bread = 10;
 console.log((milk + potato + apple + bread) * 0.9);
+
+// 5
+console.log(Math.floor(2.5));
+
+// 6
+console.log(Number.parseFloat('3.54px'));
+
+// 7
+console.log(Number.parseInt('3.54px'));
+
+// 8
+console.log(Math.sqrt(36));
+
+// 9
+let num = 3;
+let str = '3';
+console.log(parseInt(str));
+console.log(num.toString());
